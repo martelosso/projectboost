262287511e230cdba3b1d2a2f7040ff37258a177
G. Martelosso <gabriel.martelosso@gmail.com>
2023-12-09 16:30:09 -0300

    Level changes

--- Player.gd
+++ Player.gd
@@ -1,12 +1,14 @@
-extends Node3D
+extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+		apply_central_force(basis.y * delta * 1000.0)
 
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+	if Input.is_key_pressed(KEY_A):
+		apply_torque(Vector3(0.0, 0.0, 1.0) * delta * 100.0)
 
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_key_pressed(KEY_D):
+		apply_torque(Vector3(0.0, 0.0, -1.0) * delta * 100.0)
+
+
